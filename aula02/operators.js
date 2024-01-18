53d5c75031ab6f42657ca5f0e9b39af9eab99058
Matheus <mrochasandim75@gmail.com>
2024-01-18 20:30:19 -0300

    feat(jsbase): study operators

---
+++ aula02/operators.js
@@ -0,0 +1,27 @@
+//Operadores javascript
+
+// Soma -> somar numeros e concatenas strings: +
+// Subtração -> subtrair numeros: -
+// Divisão: /
+// Multiplicação: *
+// Exponenciação: **
+// Resto: %
+// console.log(5 % 2, 17 % 3)
+
+// Operadores lógicos -> operadores lógicos retornam sempre um boolean
+// Igualdade -> ==
+// console.log("5" == 5)
+// Diferença -> !=
+// console.log("5" != 3)
+// Igualdade estrita -> ===
+// console.log("5" === 5) // não compara apenas valor, compara tambem o tipo.
+// Diferença estrita -> !==
+// console.log("5" !== 5)
+// console.log(5 !== 5)
+// Ou -> ||
+console.log(2 == 2 || 1 == 2)
+// E -> &&
+console.log(2 == 2 && 1 == 2)
+// >, >=, <, <=
+console.log(2 < 3, 2 <= 3, 2 > 2, 2 >= 2)
+

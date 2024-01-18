f4ee252bc664f44bfd6d1c310a464837dd798e11
Matheus <mrochasandim75@gmail.com>
2024-01-18 20:31:21 -0300

    feat(jsbase): study variables

---
+++ aula02/variables.js
@@ -0,0 +1,13 @@
+// Variáveis
+// const são variáveis constante, ou seja, eu não posso alterar seu valor inicial
+const myName = "Matheus Rocha"
+console.log(myName)
+
+// let e var são variáis que eu posso alterar o valor inicial delas
+// let -> escopo mais estrito
+let age = 29
+console.log(age)
+
+// var -> escopo global
+var wife = "Aline Santos"
+console.log(wife)

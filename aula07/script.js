224cc5ec7255fb400358a31a904623d87df17024
Matheus <mrochasandim75@gmail.com>
2024-02-07 20:28:23 -0300

    formulário de cep

--- aula07/script.js
+++ aula07/script.js
@@ -1,8 +1,29 @@
 const cep = document.getElementById('cep')
-const inputName = document.getElementById('name')
+const cidade = document.getElementById('cidade')
+const rua = document.getElementById('rua')
+const uf = document.getElementById('uf')
+const bairro = document.getElementById('bairro')
+const numero = document.getElementById('numero')
 
-inputName.addEventListener('keypress', (event) => {
-    if(event.key === "Enter"){
-        cadastrarCEP()
+async function consultaCep(cep) {
+    try {
+        const viaCepJson = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const data = await viaCepJson.json()
+        preencheFormulario(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function preencheFormulario(data){
+    cidade.value = data.localidade
+    rua.value = data.logradouro
+    uf.value = data.uf
+    bairro.value = data.bairro
+}
+
+cep.addEventListener('keypress', (event) => {
+    if (event.key === "Enter") {
+        consultaCep(event.target.value)
     }
 })

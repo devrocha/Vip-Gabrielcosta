baff61416a41aca0dd070f878e7f63e4248a8625
Matheus <mrochasandim75@gmail.com>
2024-03-06 20:59:30 -0300

    vue

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {  ref } from 'vue';
+
+const name = ref()
+const age = ref()
+
+function newPerson() {
+  const person = {
+    name: name.value,
+    age: age.value
+  }
+
+  console.log(person)
+}
+</script>
+
+<template>
+  <input type="text" placeholder="Nome" v-model="name" />
+  <input type="number" placeholder="Idade" v-model="age">
+
+  <button @click="newPerson">Cadastrar</button>
+</template>

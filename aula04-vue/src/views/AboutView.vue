7dc9bdd2ac3e856a1d17d2ab4510037e7305dfee
Matheus <mrochasandim75@gmail.com>
2024-03-13 20:33:01 -0300

    computeds

--- aula04-vue/src/views/AboutView.vue
+++ aula04-vue/src/views/AboutView.vue
@@ -1,37 +1,45 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const grade = ref();
 const grades = ref([]);
-const bestGradeStudent = ref();
-const worstGradeStudent = ref();
-const average = ref();
 const gradeSum = ref(0);
 
+const situation = ref('')
+const className = ref()
+
 function newGrade() {
+  if (grade.value < 0 || grade.value > 10) {
+    return alert('A notas deve estar entre 0 e 10')
+  }
+
   grades.value.push(grade.value)
   gradeSum.value += grade.value
 
-  bestGrade();
-  worstGrade();
-  averageGrade();
+  studentSituation();
 }
 
-function bestGrade(){
-  bestGradeStudent.value = Math.max(...grades.value) 
-}
+const bestGrade = computed(() => grades.value.length ? Math.max(...grades.value) : '')
 
-function worstGrade(){
-  worstGradeStudent.value = Math.min(...grades.value)
-}
+const worstGrade = computed(() => grades.value.length ? Math.min(...grades.value) : '')
 
-function averageGrade(){
-  average.value = gradeSum.value/grades.value.length
-}
+const averageGrade = computed(() => grades.value.length ? gradeSum.value / grades.value.length : '')
 
-function studentSituation(){
-  if(average.value <= 4)
-    
+function studentSituation() {
+  if (averageGrade.value < 4) {
+    situation.value = 'Reprovado'
+    className.value = 'faildClass'
+    return
+  }
+
+  if (averageGrade.value >= 4 && averageGrade.value < 7) {
+    situation.value = 'Recuperação'
+    className.value = 'helpNeededClass'
+    return
+  }
+
+  situation.value = 'Aprovado'
+  className.value = 'approvedClass'
 }
 
 </script>
@@ -39,14 +47,41 @@ function studentSituation(){
   <input type="number" placeholder="Nota" v-model="grade">
   <button @click="newGrade">Cadastrar</button>
 
-  <p v-for="(element, index) in grades" :key="index">
-    <span>{{ element }}</span>
-  </p>
-  <p>Melhor nota: {{ bestGradeStudent }}</p>
-  <p>Pior nota: {{ worstGradeStudent }}</p>
-  <p>Média: {{ average }}</p>
-  <p>
-    Situação do aluno: 
-    <span></span>
-  </p>
+  <div class="grades">
+    <span v-show="grades.length">Notas: </span>
+    <span v-for="(element, index) in grades" :key="index">
+      {{ element }}
+    </span>
+  </div>
+  <div v-show="grades.length">
+    <p>Melhor nota: {{ bestGrade }}</p>
+    <p>Pior nota: {{ worstGrade }}</p>
+    <p>Média: {{ averageGrade }}</p>
+    <p>
+      Situação do aluno:
+      <span :class="className">{{ situation }}</span>
+    </p>
+  </div>
 </template>
+
+<style>
+.grades {
+  display: flex;
+}
+
+.grades>span {
+  padding-right: 5px;
+}
+
+.faildClass {
+  color: red;
+}
+
+.helpNeededClass {
+  color: purple;
+}
+
+.approvedClass {
+  color: green;
+}
+</style>
